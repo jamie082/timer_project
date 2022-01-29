e5c2a00754e21888d61d3e0ad4d1aa7abb904c80
jamie082 <jamiemorrissey02@gmail.com>
2022-01-29 13:48:41 -0800

    updated source

--- timer_project.py
+++ timer_project.py
@@ -2,47 +2,33 @@ import tkinter as tk
 from tkinter.ttk import Label
 from tkinter import messagebox
 from tkinter import *
+import time
 
-root = tk.Tk()
-root.geometry("400x200")
+root_window = tk.Tk()
+root_window.geometry("200x200")
+root_window.title("Timer by Jamie Morrissey")
 
-label = Label(root, text='0', font=('Times', 24)) # text='0'
-label.pack(ipadx=10, ipady=10)
+label = Label(root_window, text='0', font=('Times', 24)) # text='0'
 
-def StartTimer():
-    messagebox.showinfo('Message', 'You started the timer!')
+# Entry widget
 
-    inp = label.get(1.0, "end-1c")
-
-    i = 0 
-    while i <= inp:
-        print (i)
-        i = i + 1
-    print ("Good Bye!")
+e = tk.Entry(root_window)
+e.pack(padx=20, pady=20)
+e.delete(0, "end")
+e.insert(0, "Insert seconds...")
 
 # Button #1
-button = Button(root, text = "Start Timer", command = StartTimer)
+button = Button(root_window, text = "Start Timer", command = root_window.destroy)
 button.pack(side = "top")
 
-# TextBox Creation
-inputtxt = tk.Text(root, height = 3, width = 20)
-
-inputtxt.pack()
-
-# pythonguides.com/python-gui-programming/
-
-# Button #1
-#btn = Button(root, text = "Start Timer", bd = "5", command = StartTimer)
-#btn.pack(side = "top")
-
 # Button #2
-btn2 = Button(root, text="Exit Program", bd = "5", command = root.destroy ) # Exit Program
+btn2 = Button(root_window, text="Exit Program", bd = "5", command = root_window.destroy ) # Exit Program
 btn2.pack(side = "bottom")
 
 # Window maximize = False
-root.resizable(False, False)
+root_window.resizable(False, False)
 
 # show a label
 
 
-root.mainloop()
+root_window.mainloop()
